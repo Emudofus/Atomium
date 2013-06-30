cd67bc29ea214ee474878a4d46af39fb3c517b95
Blackrush <blackrushx@gmail.com>
2013-06-30 13:50:55 +0200

    creates ManyToOne and OneToMany annotations

---
+++ atomium-core/src/main/java/org/atomium/annotations/ManyToOne.java
@@ -0,0 +1,14 @@
+package org.atomium.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Blackrush
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface ManyToOne {
+}
